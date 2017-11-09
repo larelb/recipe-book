6f87cfd3c83c01bb3763d5f018a2bbf75da2e97a
Bruton, Larel <bruton2@llnl.gov>
2017-11-09 14:38:40 -0800

    Added child routing for recipes

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,21 +1,50 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from '../recipe';
 import { ShoppingListService } from '../../shopping-list/shopping-list.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'rb-recipe-detail',
   templateUrl: './recipe-detail.component.html'
 })
-export class RecipeDetailComponent implements OnInit {
-  @Input() selectedRecipe: Recipe;
+export class RecipeDetailComponent implements OnInit, OnDestroy {
+  selectedRecipe: Recipe;
+  private recipeIndex: number;
+  private subscription: Subscription;
 
-  constructor(private sls: ShoppingListService) { }
+  constructor(private sls: ShoppingListService,
+              private router: Router,
+              private route: ActivatedRoute,
+              private recipeService: RecipeService
+  ) { }
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(
+        (params: any) => {
+          this.recipeIndex = params['id'];
+          this.selectedRecipe = this.recipeService.getRecipe(this.recipeIndex);
+        }
+    );
   }
+
 
   onAddToShoppingList() {
     this.sls.addItems(this.selectedRecipe.ingredients);
+  }
+
+  onEdit() {
+    this.router.navigate(['/recipes', this.recipeIndex, 'edit']);
+  }
+
+  onDelete() {
+    this.recipeService.deleteRecipe(this.selectedRecipe);
+    this.router.navigate(['/recipes']);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
